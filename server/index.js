5b095c94392a351da25c82e105d7e67e5c1a6fad
tundexijim <freetunde2008@yahoo.com>
2023-09-13 13:45:26 +0300

    user

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const notes = require('./data/data')
+const dotenv = require('dotenv')
+const connectDB = require('./config/db')
+const userRoutes = require('./routes/userRoutes')
+const { notFound, errorHandler } = require('./middlewares/errorMiddleware')
+const app = express();
+dotenv.config();
+
+connectDB();
+app.use(express.json());
+app.get("/", (req, res) => {
+res.send("api is running..");
+})
+
+app.get("/api/notes", (req, res) => {
+res.json(notes);
+})
+    
+app.use('/api/users', userRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`server started on port ${PORT}`))
--- server/index.ts
+++
@@ -1,6 +0,0 @@
-const express = require("express")
-const dotenv = require("dotenv")
-
-const app = express();
-
-app.listen(5000, console.log("server started on port 5000"))
