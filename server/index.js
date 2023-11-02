2f14b7e477cd31f004de157dc39d7085c16b20bc
tundexijim <freetunde2008@yahoo.com>
2023-11-02 21:20:49 +0200

    deploy

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
 const noteRoutes = require("./routes/noteRoutes");
+const path = require("path");
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 const app = express();
 dotenv.config();
@@ -20,6 +21,22 @@ app.use(express.json());
 
 app.use("/api/users", userRoutes);
 app.use("/api/notes", noteRoutes);
+
+// --------------------------deployment------------------------------
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+// --------------------------deployment------------------------------
 
 app.use(notFound);
 app.use(errorHandler);
